8d245f34d4fc314eb190889d5c0df344cd615c84
gnharishkumar13 <harishkumar_gn@yahoo.com>
2021-05-01 12:27:21 -0400

    added schema migrations using golang migrate

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
@@ -17,8 +23,11 @@ type application struct {
 }
 
 type config struct {
-	port int
-	env  string
+	port     int
+	env      string
+	database struct {
+		dsn string
+	}
 }
 
 func main() {
@@ -26,6 +35,8 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 3000, " port to run the application")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.database.dsn, "db-dsn", os.Getenv("MOVIEDB_DB_DSN"), "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Llongfile|log.Ldate|log.Ltime)
@@ -34,6 +45,34 @@ func main() {
 		config: cfg,
 		logger: logger,
 	}
+
+	//database setup
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+	logger.Printf("database connection pool established")
+
+	//migrations
+
+	migrationDriver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		logger.Fatalf(err.Error(), nil)
+	}
+
+	migrator, err := migrate.NewWithDatabaseInstance("file:///path/to/your/migrations", "postgres", migrationDriver)
+	if err != nil {
+		logger.Fatalf(err.Error(), nil)
+	}
+
+	err = migrator.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		logger.Fatalf(err.Error(), nil)
+	}
+
+	logger.Printf("database migrations applied")
 
 	router := app.routes()
 
@@ -47,6 +86,31 @@ func main() {
 
 	// Start the HTTP server.
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	// Use sql.Open() to create an empty connection pool, using the DSN from the config
+	// struct.
+	db, err := sql.Open("postgres", cfg.database.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a context with a 5-second timeout deadline.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Use PingContext() to establish a new connection to the database, passing in the
+	// context we created above as a parameter. If the connection couldn't be
+	// established successfully within the 5 second deadline, then this will return an
+	// error.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the sql.DB connection pool.
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/gnharishkumar13/movie-api
 
 go 1.16
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/golang-migrate/migrate/v4 v4.14.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.1
+)
